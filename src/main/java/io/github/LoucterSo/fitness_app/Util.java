2c02500ed7ed2a504ceff6cbc591c6bd2531f07a
LoucterSo <vlad_gorelin@inbox.ru>
2025-04-03 13:15:45 +0300

    Refactoring

--- src/main/java/io/github/LoucterSo/fitness_app/Util.java
+++ src/main/java/io/github/LoucterSo/fitness_app/Util.java
@@ -7,9 +7,9 @@ public class Util {
     public static Integer calculateDailyCalorieIntake(User user) {
         int calorieNorm;
         if (user.isMan()) {
-            calorieNorm = (int) (Math.round(66.5 + (13.75 * user.getWeightInCm()) + (5.003 * user.getHeightInCm()) - (6.755 * user.getAge())));
+            calorieNorm = (int) (Math.round(66.5 + (13.75 * user.getWeightInKg()) + (5.003 * user.getHeightInCm()) - (6.755 * user.getAge())));
         } else {
-            calorieNorm = (int) (Math.round(655 + (9.563 * user.getWeightInCm()) + (1.850 * user.getHeightInCm()) - (4.676 * user.getAge())));
+            calorieNorm = (int) (Math.round(655 + (9.563 * user.getWeightInKg()) + (1.850 * user.getHeightInCm()) - (4.676 * user.getAge())));
         }
 
         if (user.getGoal().equals(User.Goal.WEIGHT_LOSS)) {
